841e338ed0ec163cc1cde9e653fea9bda2c591df
Ege Sabanci <egesabanci@outlook.com.tr>
2024-02-24 03:54:50 +0300

    refactor: minor design refactor

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,7 +3,8 @@ import Multiselect from '../components/Multiselect';
 const Main = () => {
 	return (
 		<div className='unselectable w-screen h-auto min-h-screen bg-[#151515] overflow-x-hidden'>
-			<span className='w-full h-auto flex items-center justify-center pt-[35px]'>
+			<span className='w-full h-auto flex items-center justify-center pt-[35px] gap-x-[10px]'>
+				<img src='/icon.png' className='w-[32px] h-auto rounded-full' />
 				<h1 className='text-2xl font-bold text-white'>
 					Rick and Morty | Characters
 				</h1>
